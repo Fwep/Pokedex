41784848062e3cae8e69c41d9f9f714c53ca6484
Taran Cacacho <tarancacacho@gmail.com>
2019-08-10 13:38:20 -0700

    Add Redux dev tools to project

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 import {fetchAllPokemon} from './util/api_util';
 import {receiveAllPokemon}  from './actions/pokemon_actions';
 import {selectAllPokemon} from './reducers/selectors'
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
   window.store = store;
-  ReactDOM.render(<h1>Yo</h1>, root)
+  ReactDOM.render(<Root store={store}/>, root)
   window.fetchAllPokemon = fetchAllPokemon;
   window.selectAllPokemon = selectAllPokemon;
   window.receiveAllPokemon = receiveAllPokemon;
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,7 +2,8 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 import logger from 'redux-logger';
 import thunk from '../middleware/thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
-const configureStore = () => createStore(rootReducer, applyMiddleware(thunk, logger));
+const configureStore = () => createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, logger)));
 
 export default configureStore;
