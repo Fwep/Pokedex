9321fe2cedea6ebe5b3b9bf8387fddad251a0c7a
Taran Cacacho <tarancacacho@gmail.com>
2019-08-12 16:14:50 -0700

    Add function to fetch a single Pokemon

--- frontend/util/api_util.js
+++ frontend/util/api_util.js
@@ -1,6 +1,13 @@
 export const fetchAllPokemon = () => (
   $.ajax({
     method: 'GET',
-    url: '/api/pokemon'
+    url: 'api/pokemon'
   })
 );
+
+export const fetchPokemon = (id) => (
+  $.ajax({
+    method: 'GET',
+    url: `api/pokemon/${id}`
+  })
+)
