1161128679e74d8de1ad8e1dd4bfd607185f07a3
Taran Cacacho <tarancacacho@gmail.com>
2019-08-12 16:15:19 -0700

    Fix annoying bug (check for presence of both pokemon and pokemon item ids before returning items

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,3 +1,7 @@
 export const selectAllPokemon = ({ entities }) => {
   return Object.values(entities.pokemon)
 }
+
+export const selectPokeItems = (state, pokemon) => {
+  return (pokemon && pokemon.item_ids) ? pokemon.item_ids.map(id => state.entities.items[id]) : [];
+}
